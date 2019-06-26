bf015128dc0a268f3274ba0c962e1c44ac57ed1b
kandigx <kandigx@126.com>
2019-06-26 10:01:12 +0800

    Initial commit

---
+++ src/main/java/com/kandigx/project/Application.java
@@ -0,0 +1,16 @@
+package com.kandigx.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author kandigx
+ */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
