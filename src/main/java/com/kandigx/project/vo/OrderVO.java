64562eeadf523a328f335952afea61149cb50c88
kandigx <kandigx@126.com>
2019-07-01 09:45:58 +0800

    完善 readme.md ，明确目标

---
+++ src/main/java/com/kandigx/project/vo/OrderVO.java
@@ -0,0 +1,16 @@
+package com.kandigx.project.vo;
+
+/**
+ * @author kandigx
+ * @create 2019-06-28 11:14
+ */
+public class OrderVO {
+
+    private String orderId;
+    private String time;
+    private String goodsName;
+    private String price;
+    private String weight;
+    private String clientId;
+
+}
