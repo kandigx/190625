c66c54b017188eaeb4eeec0e500bd0a4f9500400
kandigx <kandigx@126.com>
2019-07-10 16:15:56 +0800

    添加验证程序中的 thread pool 配置信息

---
+++ src/main/java/com/kandigx/project/service/ThreadPoolService.java
@@ -0,0 +1,13 @@
+package com.kandigx.project.service;
+
+/**
+ * 线程池
+ *
+ * @author kandigx
+ * @date 2019-07-10 15:53
+ */
+public interface ThreadPoolService {
+
+
+
+}
---
+++ src/main/java/com/kandigx/project/service/impl/ValidThreadPoolServiceImpl.java
@@ -0,0 +1,11 @@
+package com.kandigx.project.service.impl;
+
+import com.kandigx.project.service.ThreadPoolService;
+
+/**
+ * @author kandigx
+ * @date 2019-07-10 15:59
+ */
+public class ValidThreadPoolServiceImpl implements ThreadPoolService {
+
+}
