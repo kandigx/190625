379ce20950c880a2fedc8014559e28775bb0b473
kandigx <kandigx@126.com>
2019-07-24 13:52:34 +0800

    增加rabbitmq配置信息

--- mqserver/src/main/java/com/kandigx/mqserver/config/RabbitConfiguration.java
+++ mqserver/src/main/java/com/kandigx/mqserver/config/RabbitConfiguration.java
@@ -1,10 +1,19 @@
 package com.kandigx.mqserver.config;
 
+import com.rabbitmq.client.Channel;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.AbstractConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.api.ChannelAwareMessageListener;
+import org.springframework.amqp.support.ConsumerTagStrategy;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.UUID;
+
 
 /**
  * @author kandigx
@@ -20,23 +29,54 @@ public class RabbitConfiguration {
         this.concurrentConsumerNum = concurrentConsumerNum;
     }
 
+
     @Bean
-    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
-        RabbitAdmin rabbitAdmin = new RabbitAdmin(connectionFactory);
-        rabbitAdmin.setAutoStartup(true);
-        return rabbitAdmin;
+    public SimpleMessageListenerContainer messageListenerContainer(ConnectionFactory connectionFactory) {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer(connectionFactory);
+
+        container.setQueues(queue001(),queue002());
+        container.setConcurrentConsumers(1);
+        container.setMaxConcurrentConsumers(5);
+        container.setDefaultRequeueRejected(false);
+        container.setAcknowledgeMode(AcknowledgeMode.AUTO);
+        container.setConsumerTagStrategy((queue) -> queue + "_" + UUID.randomUUID().toString());
+        container.setMessageListener((ChannelAwareMessageListener) (message, channel) -> {
+            String msg = new String(message.getBody());
+            System.out.println("消费者：" + msg );
+        });
+
+        return container;
     }
 
+    @Bean
+    public TopicExchange exchange001(){
+        return new TopicExchange("topic001", true, false);
+    }
 
-//    @Bean
-//    public SimpleRabbitListenerContainerFactory listenerContainer() {
-//
-//        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
-//
-//        factory.createListenerContainer();
-//
-//        return factory;
-//    }
+    @Bean
+    public Queue queue001(){
+        return new Queue("queue001", true);
+    }
+
+    @Bean
+    public Binding binding001(){
+        return BindingBuilder.bind(queue001()).to(exchange001()).with("spring.*");
+    }
+
+    @Bean
+    public Queue queue002(){
+        return new Queue("queue002", true);
+    }
+
+    @Bean
+    public TopicExchange exchange002(){
+        return new TopicExchange("topic002", true, false);
+    }
+
+    @Bean
+    public Binding binding002(){
+        return BindingBuilder.bind(queue002()).to(exchange002()).with("rabbit.*");
+    }
 
 
 
--- mqserver/src/test/java/com/kandigx/mqserver/MQserverApplicationTests.java
+++ mqserver/src/test/java/com/kandigx/mqserver/MQserverApplicationTests.java
@@ -2,6 +2,12 @@ package com.kandigx.mqserver;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.amqp.AmqpException;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessagePostProcessor;
+import org.springframework.amqp.core.MessageProperties;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -11,6 +17,37 @@ public class MQserverApplicationTests {
 
     @Test
     public void contextLoads() {
+    }
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @Test
+    public void testMessage() {
+        MessageProperties messageProperties = new MessageProperties();
+        messageProperties.getHeaders().put("desc", "这是一个信息描述");
+        messageProperties.getHeaders().put("type", "自定义消息类型");
+        Message message = new Message("hello RabbitMQ".getBytes(), messageProperties);
+
+        rabbitTemplate.convertAndSend("topic001", "spring.amqp", message, (msg -> {
+            System.out.println("-----添加额外设置----");
+            msg.getMessageProperties().getHeaders().put("desc", "修改desc之后的信息");
+            msg.getMessageProperties().getHeaders().put("attr", "添加额外的信息");
+            return msg;
+        }
+        ));
+    }
+
+    @Test
+    public void testMessage2() {
+        MessageProperties messageProperties = new MessageProperties();
+        messageProperties.setContentType("text/plain");
+        Message message = new Message("mq 消息123".getBytes(), messageProperties);
+
+        rabbitTemplate.convertAndSend("topic001", "spring.amqp", "hello object message send 001");
+        rabbitTemplate.convertAndSend("topic002", "rabbit.abc", "hello object message send 002");
+
+        rabbitTemplate.convertAndSend("topic001", "spring.abc", message);
     }
 
 }
